c64535047289111296e6896513a2a8a8d9469a38
Whiskyrie <evandroropfilho@gmail.com>
2025-06-16 00:29:00 -0300

    feat: Adicionar módulos de inventário e distribuições ao UsersModule

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -4,9 +4,16 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { LoggingModule } from '../../common/logging/logging.module';
+import { InventoryModule } from '../inventory/inventory.module';
+import { DistributionsModule } from '../distributions/distributions.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), LoggingModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    LoggingModule,
+    InventoryModule,
+    DistributionsModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
