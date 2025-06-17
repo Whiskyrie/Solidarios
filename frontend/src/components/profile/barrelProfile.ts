2b7747f3cad69f65d7343aac9a4d45126b84f9ff
Maexar <marlon.meireles.profissional@gmail.com>
2025-06-16 21:35:01 -0300

    feat(profile): create barrel file to export profile components and types

---
+++ frontend/src/components/profile/barrelProfile.ts
@@ -0,0 +1,9 @@
+export { ProfileHeader } from "./ProfileHeader";
+export { StatCard } from "./StatsCard";
+export { MenuItem } from "./MenuItem";
+export { ImpactStatsSection } from "./ImpactStatsSection";
+export { ProfileMenu } from "./ProfileMenu";
+
+// Exportar tipos também
+export type { StatCardProps } from "./StatsCard";
+export type { MenuItemProps } from "./MenuItem";
