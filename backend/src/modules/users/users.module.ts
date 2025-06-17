89b52eb93b2d794821395067b6eca96572479bc5
Maexar <marlon.meireles.profissional@gmail.com>
2025-06-16 21:33:02 -0300

    feat(users): add Inventory and Distribution entities to UsersModule imports

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -3,10 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { Inventory } from '../inventory/entities/inventory.entity';
+import { Distribution } from '../distributions/entities/distribution.entity';
 import { LoggingModule } from '../../common/logging/logging.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), LoggingModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      Inventory, // Adicionar Inventory entity
+      Distribution, // Adicionar Distribution entity
+    ]),
+    LoggingModule,
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
