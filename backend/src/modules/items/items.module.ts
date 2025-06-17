3a2e3c85a4f652ef3354ec4e922121455ddadea8
Maexar <marlon.meireles.profissional@gmail.com>
2025-06-16 21:32:31 -0300

    feat(items): enhance ItemsModule imports to include User entity and forwardRef for UsersModule

--- backend/src/modules/items/items.module.ts
+++ backend/src/modules/items/items.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
 import { Item } from './entities/item.entity';
+import { User } from '../users/entities/user.entity';
 import { UsersModule } from '../users/users.module';
 import { LoggingModule } from '../../common/logging/logging.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Item]), UsersModule, LoggingModule],
+  imports: [
+    TypeOrmModule.forFeature([Item, User]), // Já tem User
+    forwardRef(() => UsersModule),
+    LoggingModule,
+  ],
   controllers: [ItemsController],
   providers: [ItemsService],
   exports: [ItemsService],
